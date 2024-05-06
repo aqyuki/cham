d2af5f0851ae725050704891510ef446c7732aef
aqyuki <yksk171118@gmail.com>
2024-05-06 12:02:57 +0900

    add logging and remove commented out code

--- discord/hook.go
+++ discord/hook.go
@@ -14,6 +14,7 @@ func newMessageCreateHandler(logger *zap.SugaredLogger) func(*discordgo.Session,
 	return func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		// Ignore all messages created by the bot itself
 		if m.Author.Bot {
+			logger.Info("skip message because it was created by the bot itself")
 			return
 		}
 
@@ -21,34 +22,36 @@ func newMessageCreateHandler(logger *zap.SugaredLogger) func(*discordgo.Session,
 
 		// if the message contains message link, expand it
 		links := extractMessageLinks(m.Content)
+		logger.Debug("detected message links", zap.Strings("links", links))
+
 		for _, link := range links {
 			info, err := extractMessageInfo(link)
 			if err != nil {
-				// TODO: add logging
+				logger.Error("failed to extract message info", zap.Error(err))
 				continue
 			}
 
 			// if the guild is not the same as the message, ignore it
 			if info.guild != m.GuildID {
-				// TODO: add logging
+				logger.Info("skip message because the guild is not the same as the message")
 				continue
 			}
 
 			// if the channel is nsfw, ignore it
 			citationChannel, err := s.Channel(info.channel)
 			if err != nil {
-				// TODO: add logging
+				logger.Error("failed to get channel", zap.Error(err))
 				continue
 			}
 			if citationChannel.NSFW {
-				// TODO: add logging
+				logger.Info("skip message because the channel is nsfw")
 				continue
 			}
 
 			// get the message
 			citationMsg, err := s.ChannelMessage(info.channel, info.message)
 			if err != nil {
-				// TODO: Add logging
+				logger.Error("failed to get message", zap.Error(err))
 				continue
 			}
 
@@ -74,6 +77,7 @@ func newMessageCreateHandler(logger *zap.SugaredLogger) func(*discordgo.Session,
 			}
 
 			embeds = append(embeds, embed)
+			logger.Debug("expanded message", zap.Any("embed", embed))
 		}
 		// expand the message
 		replyMsg := discordgo.MessageSend{
@@ -84,89 +88,12 @@ func newMessageCreateHandler(logger *zap.SugaredLogger) func(*discordgo.Session,
 			},
 		}
 		if _, err := s.ChannelMessageSendComplex(m.ChannelID, &replyMsg); err != nil {
-			// TODO: add logging
+			logger.Error("failed to send message", zap.Error(err))
 			return
 		}
+		logger.Info("expanded message", zap.String("channel", m.ChannelID), zap.String("message", m.ID))
 	}
 }
-
-// func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-// 	// Ignore all messages created by the bot itself
-// 	if m.Author.Bot {
-// 		return
-// 	}
-
-// 	var embeds []*discordgo.MessageEmbed
-
-// 	// if the message contains message link, expand it
-// 	links := extractMessageLinks(m.Content)
-// 	for _, link := range links {
-// 		info, err := extractMessageInfo(link)
-// 		if err != nil {
-// 			// TODO: add logging
-// 			continue
-// 		}
-
-// 		// if the guild is not the same as the message, ignore it
-// 		if info.guild != m.GuildID {
-// 			// TODO: add logging
-// 			continue
-// 		}
-
-// 		// if the channel is nsfw, ignore it
-// 		citationChannel, err := s.Channel(info.channel)
-// 		if err != nil {
-// 			// TODO: add logging
-// 			continue
-// 		}
-// 		if citationChannel.NSFW {
-// 			// TODO: add logging
-// 			continue
-// 		}
-
-// 		// get the message
-// 		citationMsg, err := s.ChannelMessage(info.channel, info.message)
-// 		if err != nil {
-// 			// TODO: Add logging
-// 			continue
-// 		}
-
-// 		// if the message has attachment(image), use it as the thumbnail of the embed
-// 		var image *discordgo.MessageEmbedImage
-// 		if len(citationMsg.Attachments) > 0 {
-// 			image = &discordgo.MessageEmbedImage{
-// 				URL: citationMsg.Attachments[0].URL,
-// 			}
-// 		}
-
-// 		embed := &discordgo.MessageEmbed{
-// 			Image: image,
-// 			Author: &discordgo.MessageEmbedAuthor{
-// 				Name:    citationMsg.Author.Username,
-// 				IconURL: citationMsg.Author.AvatarURL("64"),
-// 			},
-// 			Description: citationMsg.Content,
-// 			Timestamp:   citationMsg.Timestamp.Format(time.RFC3339),
-// 			Footer: &discordgo.MessageEmbedFooter{
-// 				Text: citationChannel.Name,
-// 			},
-// 		}
-
-// 		embeds = append(embeds, embed)
-// 	}
-// 	// expand the message
-// 	replyMsg := discordgo.MessageSend{
-// 		Embeds:    embeds,
-// 		Reference: m.Reference(),
-// 		AllowedMentions: &discordgo.MessageAllowedMentions{
-// 			Users: []string{m.Author.ID},
-// 		},
-// 	}
-// 	if _, err := s.ChannelMessageSendComplex(m.ChannelID, &replyMsg); err != nil {
-// 		// TODO: add logging
-// 		return
-// 	}
-// }
 
 var rgx = regexp.MustCompile(`https://(?:ptb\.|canary\.)?discord\.com/channels/(\d+)/(\d+)/(\d+)`)
 
