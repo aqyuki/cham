28eed2de0ca289073d2d592f39a9336f8c3039b2
aqyuki <yksk171118@gmail.com>
2024-05-06 09:04:00 +0900

    add config store

--- config/store.go
+++ config/store.go
@@ -1,4 +1,12 @@
 package config
 
+import "github.com/aqyuki/expand-bot/discord"
+
 // Store manages the configuration of the application.
-type Store struct{}
+type Store struct {
+	discord discord.Config
+}
+
+func (s Store) DiscordConfig() discord.Config {
+	return s.discord
+}
