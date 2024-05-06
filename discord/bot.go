c3e533d12a11223a6e0b78f63b6cd423f0291a9b
aqyuki <yksk171118@gmail.com>
2024-05-06 09:13:52 +0900

    add methods to control bot status

--- discord/bot.go
+++ discord/bot.go
@@ -1,6 +1,9 @@
 package discord
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/aqyuki/expand-bot/types"
 	"github.com/bwmarrin/discordgo"
 )
@@ -28,4 +31,38 @@ func NewBot(config DiscordConfigProvider) Bot {
 		client: nil,
 	}
 	return b
+}
+
+func (b *Bot) Start() error {
+	if b.client != nil {
+		return errors.New("bot is already running")
+	}
+
+	session, err := discordgo.New("Bot " + b.config.Token.Raw())
+	if err != nil {
+		return fmt.Errorf("failed to create session to discord because %w", err)
+	}
+
+	if err := b.client.Open(); err != nil {
+		return fmt.Errorf("failed to open session to discord because %w", err)
+	}
+	b.client = session
+	return nil
+}
+
+func (b *Bot) Stop() error {
+	if b.client == nil {
+		return errors.New("bot is not running")
+	}
+
+	defer b.purge()
+	if err := b.client.Close(); err != nil {
+		return fmt.Errorf("failed to close session to discord because %w", err)
+	}
+	return nil
+}
+
+// purge cleans up the bot instance.
+func (b *Bot) purge() {
+	b.client = nil
 }
