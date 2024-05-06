de73e145580782c1a83b08e227cbe1c97db8cb58
aqyuki <yksk171118@gmail.com>
2024-05-06 11:19:00 +0900

    add initalize process

--- config/store.go
+++ config/store.go
@@ -1,6 +1,11 @@
 package config
 
-import "github.com/aqyuki/expand-bot/discord"
+import (
+	"os"
+
+	"github.com/aqyuki/expand-bot/discord"
+	"github.com/aqyuki/expand-bot/types"
+)
 
 // Store manages the configuration of the application.
 type Store struct {
@@ -9,4 +14,12 @@ type Store struct {
 
 func (s Store) DiscordConfig() discord.Config {
 	return s.discord
+}
+
+func NewStore() Store {
+	return Store{
+		discord: discord.Config{
+			Token: types.SecretString(os.Getenv("DISCORD_TOKEN")),
+		},
+	}
 }
